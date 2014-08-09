f822b90fee6b925ad524b170faac6af07dc246b0
landon noss <landongn@gmail.com>
2014-08-08 22:39:15 -0700

    Initial Commit

---
+++ sdl-hello/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  sdl-hello
+//
+//  Created by thock on 8/8/14.
+//  Copyright (c) 2014 landon. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
